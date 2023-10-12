104486710d942faac2c77cefca1df828c929d956
Oluwaseun Sodimu <seun.sodimu@yahoo.com>
2023-10-11 20:03:15 -0500

    favicon update

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -6,7 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Damage Claim System<?= (!empty($title)) ? " | ".$title :"" ?> </title>
       <!-- Favicon -->
-      <link rel="shortcut icon" href="<?= base_url('assets') ?>/images/favicon.ico" />
+      <!-- <link rel="shortcut icon" href="<?= base_url('assets') ?>/images/favicon.ico" /> -->
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="<?= base_url('assets') ?>/css/bootstrap.min.css">
       <!-- Typography CSS -->
